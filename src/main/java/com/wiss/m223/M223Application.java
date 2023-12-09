4dad4ed51db6179385e7efeb2c3a896035153546
CKnuchel <christoph.knuchel@gmail.com>
2023-12-09 13:30:01 +0100

    Anpassen CorsCOnfig

--- src/main/java/com/wiss/m223/M223Application.java
+++ src/main/java/com/wiss/m223/M223Application.java
@@ -2,6 +2,9 @@ package com.wiss.m223;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class M223Application {
@@ -10,4 +13,15 @@ public class M223Application {
 		SpringApplication.run(M223Application.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/public**").allowedOrigins("http://localhost:5173");
+			}
+		};
+	}
+
 }
+
