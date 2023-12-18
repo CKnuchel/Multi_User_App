5760b13931f937714aaf9a38b688bfd1b069c402
CKnuchel <christoph.knuchel@gmail.com>
2023-12-18 11:50:52 +0100

    ADDED Tests

--- src/main/java/com/wiss/m223/Controller/ResponseController.java
+++ src/main/java/com/wiss/m223/Controller/ResponseController.java
@@ -10,6 +10,7 @@ import com.wiss.m223.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
